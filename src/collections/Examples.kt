84caa2a61131aa5e84ea045418ca619fbe1eb881
Marcin Moskala <marcinmoskala@gmail.com>
2019-06-04 12:47:57 +0200

    Add tests to flatMap

--- src/collections/Examples.kt
+++ src/collections/Examples.kt
@@ -1,6 +1,8 @@
 package collections
 
 // To prevent unintentional stdlib functions usage
+import org.junit.Assert
+import org.junit.Test
 import kotlin.collections.flatMap as stdlibFlatMap
 import kotlin.collections.forEach as stdlibForEach
 import kotlin.collections.map as stdlibMap
@@ -39,4 +41,26 @@ fun main(args: Array<String>) {
     (1..100).filter { it % 7 == 0 }
 //            .flatMap { num -> (1..5).map { num + 10 * it } }
             .forEach { println(it) }
+}
+
+class FlatMapTest {
+
+    @Test
+    fun `When empty collections are produced, flatMap returns an empty collection as well`() {
+//        Assert.assertEquals(emptyList<Int>(), (1..100).flatMap { emptyList<Int>() })
+//        Assert.assertEquals(emptyList<String>(), (1..100).flatMap { emptyList<String>() })
+    }
+
+    @Test
+    fun `When a collection with a single element produced, flatMap returns a list with this elements`() {
+        val list = List(1000) { it }
+//        Assert.assertEquals(list, list.flatMap { listOf(it) })
+//        Assert.assertEquals(list, list.flatMap { setOf(it) })
+    }
+
+    @Test
+    fun `When a few elements are produced, they are accumulated`() {
+        val elems = 1..5
+//        Assert.assertEquals(listOf(1, 101, 2, 102, 3, 103, 4, 104, 5, 105), elems.flatMap { listOf(it, it + 100) })
+    }
 }
