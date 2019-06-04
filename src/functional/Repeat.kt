f36fd5d65e42bbccf49eb578ca450b0c8f70c12e
Marcin Moskala <marcinmoskala@gmail.com>
2019-06-04 10:54:24 +0200

    Add tests to Repeat

--- src/functional/Repeat.kt
+++ src/functional/Repeat.kt
@@ -1,5 +1,8 @@
 package functional
 
+import org.junit.Assert
+import org.junit.Test
+
 private fun repeat(times: Int, action: () -> Unit) {
     TODO()
 }
@@ -15,4 +18,34 @@ fun main(args: Array<String>) {
 //            // break
 //        }
 //    }
+}
+
+class RepeatTest {
+
+    @Test
+    fun add3Str() {
+        var str = ""
+        repeat(3) {
+            str += "A"
+        }
+        Assert.assertEquals("AAA", str)
+    }
+
+    @Test
+    fun add3Int() {
+        var i = 0
+        repeat(3) {
+            i++
+        }
+        Assert.assertEquals(3, i)
+    }
+
+    @Test
+    fun add0Int() {
+        var i = 0
+        repeat(0) {
+            i++
+        }
+        Assert.assertEquals(0, i)
+    }
 }
