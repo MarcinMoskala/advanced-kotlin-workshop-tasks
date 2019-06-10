f5fe510d44a448f8e68f0c15d8421dce842833b4
Marcin Moskala <marcinmoskala@gmail.com>
2019-06-10 14:37:17 +0200

    Add pizza factories

--- src/basics/Pizza.kt
+++ src/basics/Pizza.kt
@@ -1,14 +1,18 @@
 package basics
 
 data class Pizza(
-        val cheese: Int = 0,
-        val pineapple: Int = 0,
-        val ham: Int = 0,
-        val egg: Int = 0,
-        val spinach: Int = 0
+    val cheese: Int = 0,
+    val pineapple: Int = 0,
+    val ham: Int = 0,
+    val egg: Int = 0,
+    val spinach: Int = 0
 )
 
 fun main() {
-//    val pizza = Pizza.hawaiian()
-//    assert(pizza == Pizza(cheese = 1, pineapple = 1, ham = 1))
+//    val pizza1 = Pizza.hawaiian()
+//    val pizza2 = PizzaFactory.hawaiian()
+//    val pizza3 = factory.hawaiian()
+//    assert(pizza1 == Pizza(cheese = 1, pineapple = 1, ham = 1))
+//    assert(pizza2 == Pizza(cheese = 1, pineapple = 1, ham = 1))
+//    assert(pizza3 == Pizza(cheese = 1, pineapple = 1, ham = 1))
 }
