ffc693727d4482976616657f7242726c7edb6849
Marcin Moskala <marcinmoskala@gmail.com>
2019-10-01 10:24:25 +0200

    Add callbacks task

---
+++ src/functional/Callbacks.kt
@@ -0,0 +1,73 @@
+package functional
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import java.util.*
+
+fun main(args: Array<String>) {
+    val username = "Your username"
+    val token = "Your token"
+    val org = "kotlin"
+    val req = RequestData(username, token, org)
+    val service: GitHubService = createGitHubService(req.username, req.password)
+
+
+}
+
+interface GitHubService {
+    @GET("orgs/{org}/repos?per_page=100")
+    fun getOrgReposCall(
+        @Path("org") org: String
+    ): Call<List<Repo>>
+
+    @GET("repos/{owner}/{repo}/contributors?per_page=100")
+    fun getRepoContributorsCall(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String
+    ): Call<List<User>>
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Repo(
+    val id: Long,
+    val name: String
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class User(
+    val login: String,
+    val contributions: Int
+)
+
+data class RequestData(
+    val username: String,
+    val password: String,
+    val org: String
+)
+
+fun createGitHubService(username: String, password: String): GitHubService {
+    val authToken = "Basic " + Base64.getEncoder().encode("$username:$password".toByteArray()).toString(Charsets.UTF_8)
+    val httpClient = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val original = chain.request()
+            val builder = original.newBuilder()
+                .header("Accept", "application/vnd.github.v3+json")
+                .header("Authorization", authToken)
+            val request = builder.build()
+            chain.proceed(request)
+        }
+        .build()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.github.com")
+        .addConverterFactory(JacksonConverterFactory.create(jacksonObjectMapper()))
+        .client(httpClient)
+        .build()
+    return retrofit.create(GitHubService::class.java)
+}
