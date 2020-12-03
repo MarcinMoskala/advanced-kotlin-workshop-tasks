703a2aecd578bcb3f096d0f6be22d4b37a2fc466
Marcin Moskala <marcin.moskala@allegro.pl>
2020-12-03 14:31:19 +0100

    Better tests for plus at

--- src/test/java/collections/PlusAtTest.kt
+++ src/test/java/collections/PlusAtTest.kt
@@ -1,5 +1,6 @@
 package collections
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -7,22 +8,36 @@ import kotlin.test.assertEquals
 class PlusAtTest {
 
     @Test
-    fun `Simple plusAt tests`() {
-        val list = listOf(1,2,3)
-        assertIsThrowingError { list.plusAt(-1, 100) }
-        assertIsThrowingError { list.plusAt(-100, 100) }
-        assertIsThrowingError { list.plusAt(1000, 100) }
-        assertEquals(listOf(100, 1, 2, 3), list.plusAt(0, 100))
-        assertEquals(listOf(1, 100, 2, 3), list.plusAt(1, 100))
-        assertEquals(listOf(1, 2, 3, 100), list.plusAt(3, 100))
+    fun `Simple addition to the middle adds correctly at the position`() {
+        assertEquals(listOf(1, 2, 7, 3), listOf(1, 2, 3).plusAt(2, 7))
+        assertEquals(listOf("1", "2", "7", "3"), listOf("1", "2", "3").plusAt(2, "7"))
     }
 
-    private fun <T> assertIsThrowingError(f: () -> T) {
-        try {
-            f()
-        } catch (r: Throwable) {
-            return
-        }
-        assert(false)
+    @Test
+    fun `When we add at size position, element is added at the end`() {
+        assertEquals(listOf(1, 2, 3, 7), listOf(1, 2, 3).plusAt(3, 7))
+        assertEquals(listOf("1", "2", "3", "7"), listOf("1", "2", "3").plusAt(3, "7"))
+    }
+
+    @Test
+    fun `When we add at 0, element is added at the beginning`() {
+        assertEquals(listOf(7, 1, 2, 3), listOf(1, 2, 3).plusAt(0, 7))
+        assertEquals(listOf("7", "1", "2", "3"), listOf("1", "2", "3").plusAt(0, "7"))
+    }
+
+    @Test
+    fun `When we try to insert at illegal position, IllegalArgumentException error is thrown`() {
+        assertTrue(catchError { listOf(1, 2, 3).plusAt(-1, 7) } is IllegalArgumentException)
+        assertTrue(catchError { listOf(1, 2, 3).plusAt(8, 7) } is IllegalArgumentException)
+        assertTrue(catchError { listOf(1, 2, 3).plusAt(10, 7) } is IllegalArgumentException)
+        assertTrue(catchError { listOf(1, 2, 3).plusAt(100, 7) } is IllegalArgumentException)
+
+    }
+
+    private fun catchError(f: () -> Unit): Throwable? = try {
+        f()
+        null
+    } catch (e: Throwable) {
+        e
     }
 }
