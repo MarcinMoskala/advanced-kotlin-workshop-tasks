364bf42f8eb7e5ae9ee3813c6fe6deabcf2bd80c
marcinmoskala <marcinmoskala@gmail.com>
2021-11-22 13:20:08 +0100

    Add PrimeAccessList exercise, add comments to BestStudents and PassingStudents

--- src/main/java/collections/PassingStudents.kt
+++ src/main/java/collections/PassingStudents.kt
@@ -1,3 +1,7 @@
 package collections
 
+// Implement makePassingStudentsListText method to display a list of students
+// who have got more than 15 points in the semester and a result of at least 50.
+// Display in alphabetical order (surname then name), in the format:
+// “{name} {surname}, {result}”
 fun List<Student>.makePassingStudentsListText(): String = TODO()
