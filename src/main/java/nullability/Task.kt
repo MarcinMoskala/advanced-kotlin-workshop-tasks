620e3bafecfa37a560fab13c0bb10beb02918a06
marcinmoskala <marcinmoskala@gmail.com>
2021-12-20 14:00:47 +0100

    Better comment

--- src/main/java/nullability/Task.kt
+++ src/main/java/nullability/Task.kt
@@ -7,6 +7,13 @@ interface Mailer {
     fun sendMessage(email: String, message: String)
 }
 
+// 1. Use Elvis operator for unpacking both values
+// 2. Use Elvis operator for unpacking email and smart-casting message
+// 3. Use Elvis operator smart-casting email and message
+// 4. Use if for smart-casting email and message
+// 5. Use if with return for smart-casting email and message
+// Optional: 6. Use let for email and message
+
 /*
 Send message if the message, client, personal info and email are not null.
 */
