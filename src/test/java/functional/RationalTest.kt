785084449ebe5aefb9ecadbc6b14238af1bdf255
marcinmoskala <marcinmoskala@gmail.com>
2022-11-30 10:53:32 +0100

    Better unit tests for Rational

--- src/test/java/functional/RationalTest.kt
+++ src/test/java/functional/RationalTest.kt
@@ -7,9 +7,20 @@ class RationalTest {
     @Test
     fun testIntExtension() {
         assertEquals(Rational(4, 1), 4.r())
+        for (i in 1..100) {
+            assertEquals(Rational(i, 1), i.r())
+        }
     }
 
-    @Test fun testPairExtension() {
+    @Test
+    fun testPairExtension() {
         assertEquals(Rational(2, 3), Pair(2, 3).r())
+
+        for (l in 1..10) {
+            for (r in 1..10) {
+                val p = Pair(l, r)
+                assertEquals(Rational(l, r), p.r())
+            }
+        }
     }
 }
